59966c56c4830bf56be999a1275024e5e1b03b57
iiiyashev <iliya.shevolaev@gmail.com>
2025-03-16 19:53:08 +0300

    sanctum test route

---
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json(['message' => 'Logged out']);
+    }
+}
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Services\CoinmarketcapService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -7,6 +8,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/test', function (Request $request) {
     return 'test';
 });
+
+Route::get('/user', function (Request $request) {
+    return 'user';
+})->middleware('auth:sanctum');
+
+Route::post('/login', [LoginController::class, 'login'])->name('login');
+Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::get('/price-test', function (CoinmarketcapService $coinmarketcap) {
     return $coinmarketcap->getPrice("BTC");
