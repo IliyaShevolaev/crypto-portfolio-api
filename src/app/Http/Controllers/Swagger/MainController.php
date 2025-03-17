7a34bc93034fae3f0bc554e4f78f6d5c65466169
iiiyashev <iliya.shevolaev@gmail.com>
2025-03-17 16:37:38 +0300

    Authorization in swagger dashboard

--- src/app/Http/Controllers/Swagger/MainController.php
+++ src/app/Http/Controllers/Swagger/MainController.php
@@ -6,12 +6,20 @@ use App\Http\Controllers\Controller;
 
 /**
  * @OA\Info(
- *      title="API Documentation",
- *      version="1.0"
+ *     title="API Documentation",
+ *     version="1.0"
+ * ),
+ * 
+ * @OA\SecurityScheme(
+ *     securityScheme="Sanctum Auth",
+ *     type="apiKey",
+ *     in="header",
+ *     name="Authorization",
+ *     description="Введите токен в формате 'Bearer {your-token}'"
  * ),
  * 
  * @OA\PathItem(
- *      path="/api/"
+ *     path="/api/"
  * )
  * 
  */
